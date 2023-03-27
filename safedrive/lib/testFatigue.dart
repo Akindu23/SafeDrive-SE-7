384c68de2d5de3afb081debcdc8b2694e39a33d8
dulmi <dulmi.20211494@iit.ac.lk>
2023-03-27 20:08:33 +0530

    Test Fatigue - que01

--- safedrive/lib/testFatigue.dart
+++ safedrive/lib/testFatigue.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:safedrive/question3.dart';
+import 'package:safedrive/testFatigue_Q1.dart';
 
 class TestFatigue extends StatefulWidget {
   @override
@@ -81,7 +83,7 @@ class _TestFatigueState extends State<TestFatigue> {
                         ),
                       ),
                       const Text(
-                        "You can't break if you are asleep."+"\n",
+                        "You can't break if you are asleep.",
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Color(0xff000730),
@@ -89,8 +91,27 @@ class _TestFatigueState extends State<TestFatigue> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: 25,),
-                      buildStartBtn(),
+                      SizedBox(height: 30.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => TestFatigue_Q1()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xff000730),
+                            onPrimary: Colors.white,
+                            padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 15.0),
+                          ),
+                          child: Text(
+                            'Start',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -98,3 +119,105 @@ class _TestFatigueState extends State<TestFatigue> {
      );
   }
 }
+
+
+class TestFatigue_Q1 extends StatefulWidget {
+  @override
+  _TestFatigue_Q1State createState() => _TestFatigue_Q1State();
+}
+
+class _TestFatigue_Q1State extends State<TestFatigue_Q1> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 100.0), // add some space here
+            Container(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 150.0,
+                decoration: BoxDecoration(
+                  color: Color(0xff000730),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                alignment: Alignment.centerRight,
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Center(
+                    child: Text(
+                      'Question  01',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "\n"+'Memorize this Number'+"\n"+"\n",
+                    style: TextStyle(
+                      color: Color(0xff000730),
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "42",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xff000730),
+                      fontSize: 100,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 100.0),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => TestFatigue_Q2()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff000730),
+                        onPrimary: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 15.0),
+                      ),
+                      child: Text(
+                        'Next',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
